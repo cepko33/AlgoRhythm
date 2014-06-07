31ec12bc7b12d5d319b0b51ede422cba67216377
Stefan Cepko <cepko33@yahoo.com>
2014-06-07 16:45:52 -0400

    got some meaningful data, more work done

--- main.py
+++ main.py
@@ -1,19 +1,51 @@
+from IPython.terminal.embed import InteractiveShellEmbed
 import essentia
 import essentia.streaming
 
 from essentia.standard import *
 
+ipshell = InteractiveShellEmbed()
+
 loader = essentia.standard.MonoLoader(filename = 'tom.wav')
 audio = loader()
 
-from pylab import plot, show, figure
+from pylab import *
 
 w = Windowing(type = 'hann')
 spectrum = Spectrum()
 mfcc = MFCC()
+zcr = ZeroCrossingRate()
+dish = DistributionShape()
+tctt = TCToTotal()
+fft = FFT()
 
-frame = audio[0:100]
-spec = spectrum(w(frame))
 
-plot(spec)
+# Data we need to compile:
+# ZCR +
+# Kurtosis +
+# Skewness +
+# Centroid +
+# Fourier +
+# MFCC +
+
+
+pool = essentia.Pool()
+
+for frame in FrameGenerator(audio, frameSize = 1024, hopSize = 512):
+    zero_cross = zcr(spectrum(w(frame)))
+    centroid = tctt(spectrum(w(frame)))
+    mfcc_bands, mfcc_coeffs = mfcc(spectrum(w(frame)))
+    pool.add('lowlevel.zcr', zero_cross)
+    pool.add('lowlevel.centroid', centroid)
+    pool.add('lowlevel.mfcc', mfcc_coeffs)
+    pool.add('lowlevel.mfcc_bands', mfcc_bands)
+
+#imshow(pool['lowlevel.mfcc'].T[1:,:], aspect = 'auto')
+# Let's plot mfcc bands on a log-scale so that the energy values will be better 
+# differentiated by color
+from matplotlib.colors import LogNorm
+imshow(pool['lowlevel.mfcc_bands'].T, aspect = 'auto', interpolation = 'nearest', norm = LogNorm())
 show()
+
+output = YamlOutput(filename = 'data.sig')
+output(pool)
