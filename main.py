b817d9e4086347367385405c291235b2310c9d51
Stefan Cepko <cepko33@yahoo.com>
2014-06-07 17:01:41 -0400

    compiles 5 different arrays of data

--- main.py
+++ main.py
@@ -35,10 +35,13 @@ for frame in FrameGenerator(audio, frameSize = 1024, hopSize = 512):
     zero_cross = zcr(spectrum(w(frame)))
     centroid = tctt(spectrum(w(frame)))
     mfcc_bands, mfcc_coeffs = mfcc(spectrum(w(frame)))
-    pool.add('lowlevel.zcr', zero_cross)
     pool.add('lowlevel.centroid', centroid)
     pool.add('lowlevel.mfcc', mfcc_coeffs)
     pool.add('lowlevel.mfcc_bands', mfcc_bands)
+
+for entry in fft(audio):
+    pool.add('fft', float(entry))
+pool.add('zcr', zcr(audio))
 
 #imshow(pool['lowlevel.mfcc'].T[1:,:], aspect = 'auto')
 # Let's plot mfcc bands on a log-scale so that the energy values will be better 
